b4df7df2ee5b9f64310f45fc68f9303b2912c9b5
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-01-04 13:11:36 -0600

    Trial build result of 4794a41 from develop

--- lib/Modware/Loader.pm
+++ lib/Modware/Loader.pm
@@ -40,12 +40,12 @@ developer release,  suit your script accordingly if you want to use the stable o
 =item Single step
 
   $_> curl -o BioPortal-WebService.tar.gz -L -k \
-       https://github.com/dictyBase/BioPortal-WebService/archive/build/develop.tar.gz && \
-       cpanm -n BioPortal-WebService.tar.gz  && \
-       curl -o Modware-Loader.tar.gz -L -k \
-       https://github.com/dictyBase/Modware-Loader/archive/build/develop.tar.gz && \
-       cpanm -n Modware-Loader.tar.gz && \
-       rm BioPortal-WebService.tar.gz Modware-Loader.tar.gz
+     https://github.com/dictyBase/BioPortal-WebService/archive/build/develop.tar.gz && \
+     cpanm -n BioPortal-WebService.tar.gz  && \
+     curl -o Modware-Loader.tar.gz -L -k \
+     https://github.com/dictyBase/Modware-Loader/archive/build/develop.tar.gz && \
+     cpanm -n Modware-Loader.tar.gz && \
+     rm BioPortal-WebService.tar.gz Modware-Loader.tar.gz
 
 =item Manually
 
@@ -65,14 +65,10 @@ This is primarilly intended for authors/developers.
 =over
 
     git checkout git://github.com/dictyBase/Modware-Loader.git
-
     cpanm -n Dist::Zilla
-
     curl -o BioPortal-WebService.tar.gz -L -k \
        https://github.com/dictyBase/BioPortal-WebService/archive/build/develop.tar.gz && \
-
     dzil listdeps --author --missing | cpanm -n
-
     dzil install
 
 =back
