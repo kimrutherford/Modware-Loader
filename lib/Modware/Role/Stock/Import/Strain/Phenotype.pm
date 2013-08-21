848e841e2ebb09fb591d83da46e4b9befbde6389
Yogesh Pandit <yogesh.pandit@northwestern.edu>
2013-08-21 16:20:24 -0500

    Preliminary code for importing stock phenotype

---
+++ lib/Modware/Role/Stock/Import/Strain/Phenotype.pm
@@ -0,0 +1,19 @@
+
+package Modware::Role::Stock::Import::Strain::Phenotype;
+
+use strict;
+
+use Moose;
+use namespace::autoclean;
+
+1;
+
+__END__
+
+=head1 NAME
+=head1 VERSION
+=head1 SYNOPSIS
+=head1 DESCRIPTION
+=head1 AUTHOR
+=head1 LICENSE AND COPYRIGHT 
+=cut
